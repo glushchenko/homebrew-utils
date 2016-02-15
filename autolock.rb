cbecf22723ed4c01aa84fc12007782901a2af9f8
Oleksandr Glushchenko <contact@fluder.co>
2016-02-15 22:38:27 +0200

    checksum fix

--- autolock.rb
+++ autolock.rb
@@ -2,7 +2,7 @@ class Autolock < Formula
   desc "OS X auto locker when lid closed"
   homepage "https://github.com/glushchenko/mac-autolock"
   url "https://github.com/glushchenko/mac-autolock/archive/1.0.tar.gz"
-  sha256 "b622f55b35abd9b0f6e9fc96c6e4adf1b7739cbde8b3a182cd24bbe206aa3c5c"
+  sha256 "c1c89c41ccd3f50db88e7dbbefa9f28e8867d6d96d761b26832e54020024ce81"
 
   bottle :unneeded
 
